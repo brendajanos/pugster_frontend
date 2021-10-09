ecf4fcad94998c96f2fe79b210198f582678bf62
brendajanos <brenda.janos@icloud.com>
2021-10-09 22:15:37 +0200

    map ok in device

--- src/Map.ios.tsx
+++ src/Map.ios.tsx
@@ -2,7 +2,9 @@ import React, {useEffect} from 'react';
 import {StyleSheet} from 'react-native';
 import MapboxGL from '@react-native-mapbox-gl/maps';
 
-MapboxGL.setAccessToken(process.env.MAPBOX_TOKEN as string);
+MapboxGL.setAccessToken(
+  'pk.eyJ1IjoicHVnc3RlciIsImEiOiJja3RzdmxydnUxazJmMnVtcG0yMGNma3YyIn0.PgJQ_Tqd4FGeHspfGMOcNQ',
+);
 
 const styles = StyleSheet.create({
   map: {
